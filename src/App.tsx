416529583914207daf1d157c9041bfc0d556709e
sparkie11 <gautamwise.s@gmail.com>
2024-10-30 17:06:42 +0530

    d

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { useState } from 'react';
 import './App.css';
+import words from './wordList.json';
 
 function App() {
 
-  const [wordToGuess, setWordToGuess] = useState("Test" )
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+
+  const [guessedLetters, setGuessedLetters] = useState([]);
+
+  console.log(wordToGuess);
 
   return (
     <div className="App">
